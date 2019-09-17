4e9075372f28fe8580369c83cf5df446987c8532
Vasudev Singh <vasu.singh002@gmail.com>
2019-09-17 21:39:45 +0530

    Face_detecttion

---
+++ face_detector.py
@@ -0,0 +1,124 @@
+from __future__ import print_function
+import os
+import argparse
+import torch
+import torch.backends.cudnn as cudnn
+import numpy as np
+from data import cfg
+from layers.functions.prior_box import PriorBox
+from utils.nms.py_cpu_nms import py_cpu_nms
+import cv2
+from models.retinaface import RetinaFace
+from utils.box_utils import decode, decode_landm
+from utils.timer import Timer
+
+torch.set_grad_enabled(False)
+net = RetinaFace(phase="test")
+
+CONFIDENCE = 0.50
+NMS_THRESHOLD = 0.30
+VIZ_THRESHOLD = 0.50
+
+resize = 1
+
+
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+def load_model(model, pretrained_path):
+    pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+net = load_model(net, "Final_Retinaface.pth")
+net.eval()
+cudnn.benchmark = True
+device = torch.device("cpu")
+net = net.to(device)
+
+def face_detector(frame):
+    img_raw = frame.copy()
+    img = np.float32(img_raw)
+    if resize != 1:
+        img = cv2.resize(img, None, None, fx=resize, fy=resize, interpolation=cv2.INTER_LINEAR)
+    im_height, im_width, _ = img.shape
+    scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+    img -= (104, 117, 123)
+    img = img.transpose(2, 0, 1)
+    img = torch.from_numpy(img).unsqueeze(0)
+    img = img.to(device)
+    scale = scale.to(device)
+
+    loc, conf, landms = net(img)  # forward pass
+    priorbox = PriorBox(cfg, image_size=(im_height, im_width))
+    priors = priorbox.forward()
+    priors = priors.to(device)
+    prior_data = priors.data
+    boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
+    boxes = boxes * scale / resize
+    boxes = boxes.cpu().numpy()
+    scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+    landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
+    scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                            img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                            img.shape[3], img.shape[2]])
+    scale1 = scale1.to(device)
+    landms = landms * scale1 / resize
+    landms = landms.cpu().numpy()
+
+        # ignore low scores
+    inds = np.where(scores > CONFIDENCE)[0]
+    boxes = boxes[inds]
+    landms = landms[inds]
+    scores = scores[inds]
+
+        # keep top-K before NMS
+    order = scores.argsort()[::-1][:5000]
+    boxes = boxes[order]
+    landms = landms[order]
+    scores = scores[order]
+
+        # do NMS
+    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+    keep = py_cpu_nms(dets, NMS_THRESHOLD)
+        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+    dets = dets[keep, :]
+    landms = landms[keep]
+
+        # keep top-K faster NMS
+    dets = dets[:750, :]
+    landms = landms[:750, :]
+
+    dets = np.concatenate((dets, landms), axis=1)
+
+    for b in dets:
+        if b[4] < VIZ_THRESHOLD:
+            continue
+        text = "{:.4f}".format(b[4])
+        b = list(map(int, b))
+        cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+        
+    name = "out.jpg"
+    cv2.imwrite(name, img_raw)
+
+
+
+frame = cv2.imread("/home/vasu/Downloads/aaaaaaa.jpeg")
+face_detector(frame)
